12ee07382d296bac78b827b2d89fc70c0d38c88b
risnite <farisabdulhakim@gmail.com>
2023-03-16 07:58:57 +0700

    added dotenv

--- controllers/customer-controller.js
+++ controllers/customer-controller.js
@@ -2,10 +2,10 @@ module.exports = {
   index: (req, res) => {
     const mysql = require('mysql')
     const connection = mysql.createConnection({
-      host: 'localhost',
-      user: 'admin',
-      password: 'admin',
-      database: 'farisweb_umkmku'
+      host: process.env.DB_HOST,
+      user: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE
     })
 
     connection.connect()
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 var expressLayouts = require('express-ejs-layouts');
+// dotenv
+require('dotenv').config()
 
 const app = express();
 const port = 5000;
